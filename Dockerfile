a69ff4a947fd338cdcb6150902072a26acc1b750
Thomas Kint <thomaskint.pro@gmail.com>
2018-07-02 22:24:45 +0200

    Ajout de la gestion du volume docker (stockage externe au container)

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
+RUN mkdir /usr/src/databases
+VOLUME /usr/src/databases
+
 COPY requirements.txt ./
 
 RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip
-
 RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --no-cache-dir -r requirements.txt
 
 COPY . .
@@ -13,6 +15,5 @@ COPY . .
 ENV FLASK_APP='./src/main.py'
 
 EXPOSE 8500 8500
-EXPOSE 5000 5000
 
 CMD [ "python", "-m", "flask", "run" ]
--- scriptDocker.sh
+++ scriptDocker.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
-echo "######################"
-echo "Starting KiboDB Docker"
-echo "######################"
+echo "##########################"
+echo "# Starting KiboDB Docker #"
+echo "##########################"
 echo ""
 
 if [ "$(docker ps -a | grep kibodb-run)" ]
-then 
+then
     echo "Docker kibodb already running"
     echo ""
+
     echo "Docker rm kibodb-run: "
     echo ""
-    docker rm kibodb-run
+    docker rm -f kibodb-run
+    echo ""
+
+    echo "Docker rmi kibodb"
+    echo ""
+    docker rmi -f kibodb
+    echo ""
 fi
 
-echo "Docker build : "
 echo ""
+echo "################"
+echo "# Docker build #"
+echo "################"
+echo ""
+
 docker build . --tag kibodb
 
 echo ""
-echo "Docker run : "
+echo "#################"
+echo "# Creating dirs #"
+echo "#################"
 echo ""
-docker run --name kibodb-run -p 8500:8500 -d kibodb
 
-echo "Done"
+mkdir /var/kibodb
+mkdir /var/kibodb/databases
+
+echo ""
+echo "##############"
+echo "# Docker run #"
+echo "##############"
+echo ""
+
+docker run -v /var/kibodb/databases:/usr/src/databases --name kibodb-run -p 8500:8500 -d kibodb
+
+echo ""
+echo "#########################"
+echo "# KiboDB is now running #"
+echo "#########################"
+echo ""
 
 docker logs kibodb-run
 
