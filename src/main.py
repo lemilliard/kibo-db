688eb3fd7498224809bdfc157efadcc7c92e6f3e
Thomas Kint <thomaskint.pro@gmail.com>
2018-08-02 14:01:07 +0200

    Creation branche develop et fix main

--- src/main.py
+++ src/main.py
@@ -9,5 +9,4 @@ app.config["DEBUG"] = True
 app.register_blueprint(admin_api, url_prefix="/admin")
 app.register_blueprint(query_api, url_prefix="/query")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=Config.port)
+app.run(host="0.0.0.0", port=Config.port)
