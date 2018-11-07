48322e86feaf6e439e8ed33e66bfe5e5b38a7005
Kevin Bouzan <kevin.bouzan@gmail.com>
2018-11-07 17:11:27 +0100

    get sous schema by key

--- src/main/fsp/steps/test.py
+++ src/main/fsp/steps/test.py
@@ -8,11 +8,44 @@ string = "{id1, liste_objets: {id2, test: {id5}}, sous_objet: {id3}}"
 # if search is not None:
 #     print(search.group())
 
+# def get_sous_schema(schema, key):
+#     cpt = 0
+#     tmp_sous_schema = ""
+#     sous_schema = ""
+#     in_object = False
+#     in_key = False
+#     tmp_key = ""
+#     for c in string[1:-1]:
+#         if c == "{":
+#             in_object = True
+#             cpt += 1
+#         elif c == "}":
+#             cpt -= 1
+#         if in_object:
+#             tmp_sous_schema += c
+#             if cpt == 0:
+#                 if key == tmp_key:
+#                     sous_schema = tmp_sous_schema
+#                 in_object = False
+#                 in_key = False
+#                 tmp_sous_schema = ""
+#                 tmp_key = ""
+#         else:
+#             if c in [",", " "] and not in_key:
+#                 tmp_key = ""
+#             else:
+#                 if c == ":":
+#                     in_key = True
+#                 if not in_key:
+#                     tmp_key += c
+#     return sous_schema
 
-def get_sous_schema(schema):
+def get_sous_schema(schema, key):
     cpt = 0
     sous_schema = ""
     in_object = False
+    in_key = False
+    tmp_key = ""
     i = 1
     while i in range(1, schema.__len__() - 1):
         c = schema[i]
@@ -24,9 +57,46 @@ def get_sous_schema(schema):
         if in_object:
             sous_schema += c
             if cpt == 0:
-                i = schema.__len__()
+                if key == tmp_key:
+                    i = schema.__len__()
+                else:
+                    in_object = False
+                    in_key = False
+                    sous_schema = ""
+                    tmp_key = ""
+        else:
+            if c in [",", " "] and not in_key:
+                tmp_key = ""
+            else:
+                if c == ":":
+                    in_key = True
+                if not in_key:
+                    tmp_key += c
         i += 1
     return sous_schema
 
+# def get_sous_schema(schema):
+#     cpt = 0
+#     sous_schema = ""
+#     in_object = False
+#     i = 1
+#     while i in range(1, schema.__len__() - 1):
+#         c = schema[i]
+#         if c == "{":
+#             in_object = True
+#             cpt += 1
+#         elif c == "}":
+#             cpt -= 1
+#         if in_object:
+#             sous_schema += c
+#             if cpt == 0:
+#                 i = schema.__len__()
+#         i += 1
+#     return sous_schema
+#
+#
+# print(get_sous_schema(string))
 
-print(get_sous_schema(string))
+
+# print(get_sous_schema(string, "liste_objets"))
+print(get_sous_schema(string, "sous_objet"))
